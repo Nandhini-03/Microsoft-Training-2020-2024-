67e06d96aeaa914b112c0872eab51b1b2735669c
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-10-16 17:20:20 +0530

    Create 24.Sqrt(x)-69.java

---
+++ Google Assignments/Assignment 1/24.Sqrt(x)-69.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int s=1,e=x;
+        while(s<e){
+            int mid=s+(e-s) / 2;
+            if(mid<=x / mid && (mid+1)>x / (mid+1))
+                  return mid;
+            else if(mid>x / mid)
+                e=mid;
+            else
+                s=mid+1;
+        }
+        return s;
+    }
+}
+/*
+Example 1:
+Input: x = 4
+Output: 2
+Example 2:
+Input: x = 8
+Output: 2 */
