fd944328ddaa1fb355af437dd0e76acf81f6eee1
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-10-27 12:39:53 +0530

    Update 152. Maximum Product Subarray.java

--- Frequently asked questions/Array/152. Maximum Product Subarray.java
+++ Frequently asked questions/Array/152. Maximum Product Subarray.java
@@ -1,1 +1,30 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int product=1;
+        int result=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            product=product*nums[i];
+            result=Math.max(product,result);
+            if(product==0)
+            {
+                product=1;
+            }
+        }
+        product=1;
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            product=product*nums[i];
+            result=Math.max(product,result);
+            if(product==0)
+            {
+                product=1;
+            }
+        }
+        return result;
+    }
+}
 
+/*Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.  */
