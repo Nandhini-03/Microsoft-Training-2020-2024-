f04c11fb403748a5e0101dc4b28c0ae73279685c
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-10-31 21:19:11 +0530

    Create 371. Sum of Two Integers.java

---
+++ Frequently asked questions/Binary/371. Sum of Two Integers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getSum(int a, int b) {
+       while(b!=0){
+           int temp=(a&b)<<1;   // and operator and shift 1.
+           a=a^b;   // ^ -> xor operator
+           b=temp;
+       } 
+        return a;
+    }
+}
+/*
+Example 1:
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+Input: a = 2, b = 3
+Output: 5
+*/
