4750da70410e8dd578e2a9670517a1a9539936fa
Nandhini-03 <108286273+Nandhini-03@users.noreply.github.com>
2022-11-01 12:38:52 +0530

    Create 15. 3Sum.java

---
+++ Question Bank Problems/Apple/15. 3Sum.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) 
+    {
+        List<List<Integer>> list=new ArrayList<>();
+        Arrays.sort(nums);
+        int n =nums.length;
+        if(n<3){
+            return list;
+        }
+        for(int i=0;i<=n-3;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int left=i+1,right=n-1;
+            int sum=0-nums[i];
+            while(left<right){
+                if(sum==nums[left]+nums[right]){
+                    List<Integer> l=new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[left]);
+                    l.add(nums[right]);
+                    list.add(l);  
+                    while(left<right && nums[left]==nums[left+1]){
+                        left++;
+                    }
+                    
+                    while(left<right && nums[right]==nums[right-1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+                else if(nums[left]+nums[right]<sum){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return list;
+    }
+}
